9715d3dc270269ebebdcdc192062ef47a4b31c7c
gongzhen <gongzhengz@gmail.com>
2017-08-07 08:14:04 -0700

    417

---
+++ breadth-first search/417_PacificAtlanticWaterFlow/Solution1.java
@@ -0,0 +1,137 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+// class TreeNode {
+//  int val;
+//  TreeNode left;
+//  TreeNode right;
+//  TreeNode(int x) { val = x; }
+// }
+// private TreeNode createTree() {
+//  //       5
+//  //    2      7
+//  //  1   3      10
+//  TreeNode node5 = new TreeNode(5);
+//  TreeNode node2 = new TreeNode(2);
+//  TreeNode node1 = new TreeNode(1);
+//  TreeNode node7 = new TreeNode(7);
+//  TreeNode node10 = new TreeNode(10);
+
+//  node5.left = node2;
+//  node5.right = node7;
+//  node2.left = node1;
+//  node2.right = node3;
+//  node5.right = node7;
+//  node7.right = node10; 
+//  return node5;
+// }
+// private void printList(List<String> list) {
+//  for(String s: list) {
+//      System.out.println(s);
+//  }
+// }
+// private void printQueue(Queue<String> list) {
+//  for(String s: list) {
+//      System.out.println(s);
+//  }
+// } 
+// private void printListOfList(List<List<Integer>> listOflist) {
+//  for(List<Integer> list: listOflist){
+//      for(int n : list) {
+//          System.out.print(n + ", ");
+//      }
+//      System.out.println();
+//  }
+// }
+// private void printMatrix(int[][] matrix) {
+//     for(int[] s: matrix) {
+//         for(int n: s) {
+//             System.out.print(n + ",");
+//         }
+//         System.out.println();            
+//     }
+// }  
+//  private void scannerIn() {
+//    Scanner scanner = new Scanner(System.in);
+//    scanner.nextLine();
+// }
+// private void printString(String arg) {
+//     System.out.println(arg); 
+// }
+// private void printLine() {
+//     System.out.println("---------------------"); 
+// }  
+// 
+// private void printMap(HashMap<TreeNode, Integer> map) {
+//     for(TreeNode node:map.keySet()) {
+//         Integer value = map.get(node);
+//         printString("key:" + node.val + ",value:" + value);
+//     }
+// }
+// private void printStringWithoutNewLine(String arg) {
+//     System.out.print(arg + ","); 
+// } 
+// private void printArray(int[] list) {
+//     for(int n: list) {
+//         System.out.println(n);
+//     }
+// } 
+// private void printTree(TreeNode root) {
+//     if(root == null) { return ; }
+//     Queue<TreeNode> queue = new LinkedList<TreeNode>();
+//     queue.offer(root);
+//     while(!queue.isEmpty()) {
+//         int size = queue.size();
+//         for(int i = 0; i < size; i++) {
+//             TreeNode node = queue.poll();                
+//             if(node.left != null) {
+//                 queue.offer(node.left);
+//             }
+//             if(node.right != null) {
+//                 queue.offer(node.right);
+//             }                
+//             printStringWithoutNewLine("" + node.val);
+//         }
+//         printLine();
+//     }
+// } 
+
+public class Solution1 {
+
+    public List<int[]> pacificAtlantic(int[][] matrix) {
+        List<int[]> result = new ArrayList<int[]>();
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean[][] PC = new boolean[m][n];
+        boolean[][] AT = new boolean[m][n];
+        for(int i = 0; i < m; i++) {
+            if(i == 0) {
+                PC[]
+            }
+        }
+    }
+
+    private void printLine() {
+    	System.out.println("---------------------"); 
+	}
+
+    private void printString(String arg) {
+        System.out.println(arg); 
+    }    
+
+	private void printList(int[] list, int length) {
+		for(int i = 0; i < length; i ++ ){
+			System.out.println(list[i]);
+		}
+	}    
+
+	public static void main(String[] args) {
+		Solution1 obj = new Solution1();
+		int[][] matrix = new int[][]{{1,2,2,3,5},{3,2,3,4,4},{2,4,5,3,1},{6,7,1,4,5},{5,1,1,2,4}};
+		System.out.println(obj.pacificAtlantic(matrix));
+	}
+
+}
