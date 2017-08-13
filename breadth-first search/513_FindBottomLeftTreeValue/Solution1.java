d27ff543718d3a6b098b8e8c6aa7090dad2796ae
gongzhen <gongzhengz@gmail.com>
2017-08-12 22:42:35 -0700

    513 FindBottomLeftTreeValue bfs

---
+++ breadth-first search/513_FindBottomLeftTreeValue/Solution1.java
@@ -0,0 +1,185 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+// class TreeNode {
+//  int val;
+//  TreeNode left;
+//  TreeNode right;
+//  TreeNode(int x) { val = x; }
+// }
+// private TreeNode createTree() {
+//  //       5
+//  //    2      7
+//  //  1   3      10
+//  TreeNode node5 = new TreeNode(5);
+//  TreeNode node2 = new TreeNode(2);
+//  TreeNode node1 = new TreeNode(1);
+//  TreeNode node7 = new TreeNode(7);
+//  TreeNode node10 = new TreeNode(10);
+
+//  node5.left = node2;
+//  node5.right = node7;
+//  node2.left = node1;
+//  node2.right = node3;
+//  node5.right = node7;
+//  node7.right = node10; 
+//  return node5;
+// }
+// private void printList(List<String> list) {
+//  for(String s: list) {
+//      System.out.println(s);
+//  }
+// }
+// private void printQueue(Queue<String> list) {
+//  for(String s: list) {
+//      System.out.println(s);
+//  }
+// } 
+// private void printListOfList(List<List<Integer>> listOflist) {
+//  for(List<Integer> list: listOflist){
+//      for(int n : list) {
+//          System.out.print(n + ", ");
+//      }
+//      System.out.println();
+//  }
+// }
+// private void printMatrix(int[][] matrix) {
+//     for(int[] s: matrix) {
+//         for(int n: s) {
+//             System.out.print(n + ",");
+//         }
+//         System.out.println();            
+//     }
+// }  
+//  private void scannerIn() {
+//    Scanner scanner = new Scanner(System.in);
+//    scanner.nextLine();
+// }
+// private void printString(String arg) {
+//     System.out.println(arg); 
+// }
+// private void printLine() {
+//     System.out.println("---------------------"); 
+// }  
+// 
+// private void printMap(HashMap<TreeNode, Integer> map) {
+//     for(TreeNode node:map.keySet()) {
+//         Integer value = map.get(node);
+//         printString("key:" + node.val + ",value:" + value);
+//     }
+// }
+// private void printStringWithoutNewLine(String arg) {
+//     System.out.print(arg + ","); 
+// } 
+// private void printArray(int[] list) {
+//     for(int n: list) {
+//         System.out.println(n);
+//     }
+// } 
+// private void printTree(TreeNode root) {
+//     if(root == null) { return ; }
+//     Queue<TreeNode> queue = new LinkedList<TreeNode>();
+//     queue.offer(root);
+//     while(!queue.isEmpty()) {
+//         int size = queue.size();
+//         for(int i = 0; i < size; i++) {
+//             TreeNode node = queue.poll();                
+//             if(node.left != null) {
+//                 queue.offer(node.left);
+//             }
+//             if(node.right != null) {
+//                 queue.offer(node.right);
+//             }                
+//             printStringWithoutNewLine("" + node.val);
+//         }
+//         printLine();
+//     }
+// } 
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
+public class Solution1 {
+
+    private int leftValue;
+    private int max;
+
+    public int findBottomLeftValue(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+        leftValue = root.val;
+        max = 0;
+        dfs(root, 0);
+        return leftValue;
+    }
+
+    private void dfs(TreeNode node, int depth) {
+        if (node == null) {
+            printString("node is null");
+            printLine();
+            return;
+        }
+        printString("node:" + node.val + "'s left => depth:" + (depth + 1));
+        dfs(node.left, depth + 1);
+        printString("node:" + node.val + "'s right => depth:" + (depth + 1));
+        dfs(node.right, depth + 1);
+        if(depth > max) {
+            int tempMax = max;
+            max = depth;
+            leftValue = node.val;
+            printString("depth = " + depth + " > max:" + tempMax + ". leftValue = node's val = " + node.val);                        
+        }
+    }
+
+    private TreeNode createTree() {
+        //       5
+        //    2      7
+        //  1   3      10
+        //     4
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node7 = new TreeNode(7);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node10 = new TreeNode(10);
+        TreeNode node4 = new TreeNode(4);
+
+        node5.left = node2;
+        node5.right = node7;
+        node2.left = node1;
+        node2.right = node3;
+        node5.right = node7;
+        node7.right = node10; 
+        node3.left = node4;
+        return node5;
+    }    
+
+    private void printLine() {
+    	System.out.println("---------------------"); 
+	}
+
+    private void printString(String arg) {
+        System.out.println(arg); 
+    }    
+
+	private void printList(int[] list, int length) {
+		for(int i = 0; i < length; i ++ ){
+			System.out.println(list[i]);
+		}
+	}    
+
+	public static void main(String[] args) {
+		Solution1 obj = new Solution1();
+        TreeNode root = obj.createTree();
+		System.out.println(obj.findBottomLeftValue(root));
+	}
+
+}
