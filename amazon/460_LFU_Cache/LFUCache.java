d1bb90b6d75b51a82aa2aa373d7fa2ec2c55a55b
gongzhen <gongzhengz@gmail.com>
2017-11-03 19:42:32 -0700

    460 serialize and deserialize binary tree

---
+++ amazon/460_LFU_Cache/LFUCache.java
@@ -0,0 +1,188 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+// class TreeNode {
+//  int val;
+//  TreeNode left;
+//  TreeNode right;
+//  TreeNode(int x) { val = x; }
+// }
+// private TreeNode createTree() {
+//  //       5
+//  //    2      7
+//  //  1   3      10
+//  TreeNode node5 = new TreeNode(5);
+//  TreeNode node2 = new TreeNode(2);
+//  TreeNode node1 = new TreeNode(1);
+//  TreeNode node7 = new TreeNode(7);
+//  TreeNode node10 = new TreeNode(10);
+
+//  node5.left = node2;
+//  node5.right = node7;
+//  node2.left = node1;
+//  node2.right = node3;
+//  node5.right = node7;
+//  node7.right = node10; 
+//  return node5;
+// }
+// private void printList(List<String> list) {
+//  for(String s: list) {
+//      System.out.println(s);
+//  }
+// }
+// private void printQueue(Queue<String> list) {
+//  for(String s: list) {
+//      System.out.println(s);
+//  }
+// } 
+// private void enterKey() {
+//     System.out.println("Press \"ENTER\" to continue...");
+//     Scanner scanner = new Scanner(System.in);
+//     scanner.nextLine();      
+// }
+// private void printListOfList(List<List<Integer>> listOflist) {
+//  for(List<Integer> list: listOflist){
+//      for(int n : list) {
+//          System.out.print(n + ", ");
+//      }
+//      System.out.println();
+//  }
+// }
+// private void printMatrix(int[][] matrix) {
+//     for(int[] s: matrix) {
+//         for(int n: s) {
+//             System.out.print(n + ",");
+//         }
+//         System.out.println();            
+//     }
+// }  
+//  private void scannerIn() {
+//    Scanner scanner = new Scanner(System.in);
+//    scanner.nextLine();
+// }
+// private void printString(String arg) {
+//     System.out.println(arg); 
+// }
+// private void printLine() {
+//     System.out.println("---------------------"); 
+// }  
+// 
+// private void printMap(HashMap<TreeNode, Integer> map) {
+//     for(TreeNode node:map.keySet()) {
+//         Integer value = map.get(node);
+//         printString("key:" + node.val + ",value:" + value);
+//     }
+// }
+// private void printStringWithoutNewLine(String arg) {
+//     System.out.print(arg + ","); 
+// } 
+// private void printArray(int[] list) {
+//     for(int n: list) {
+//         System.out.println(n);
+//     }
+// } 
+// private void printStack(Stack<Integer> stack) {
+//     Interator<Integer> iter = stack.iterator();
+//     while(iter.hasNext()) {
+//         printString("" + iter.next());
+//     }
+// }
+// private void printTree(TreeNode root) {
+//     if(root == null) { return ; }
+//     Queue<TreeNode> queue = new LinkedList<TreeNode>();
+//     queue.offer(root);
+//     while(!queue.isEmpty()) {
+//         int size = queue.size();
+//         for(int i = 0; i < size; i++) {
+//             TreeNode node = queue.poll();                
+//             if(node.left != null) {
+//                 queue.offer(node.left);
+//             }
+//             if(node.right != null) {
+//                 queue.offer(node.right);
+//             }                
+//             printStringWithoutNewLine("" + node.val);
+//         }
+//         printLine();
+//     }
+// } 
+
+public class LFUCache {
+
+    private static class Node {
+
+        int key;
+        int value;
+        int freq;
+        ListIterator<Integer> itr;
+
+        public Node(int key, int value, int freq, ListIterator<Integer> itr) {
+            this.key = key;
+            this.value = val;
+            this.freq = freq;
+            this.itr = itr;
+        }
+    }
+
+    private int capacity;
+    private int minFreq;
+    private Map<Integer, Node> map;
+    private Map<Integer, List<Integer>> listMap;
+
+    public LFUCache(int capacity) {
+        this.capacity = capacity;
+        this.minFreq = 0;
+        this.map = new HashMap<Integer, Node>();
+        this.listMap = new HashMap<Integer, Integer>();
+    }
+    
+    public int get(int key) {
+        if(map.containsKey(key) == false) {
+            return -1;
+        }
+
+        Node node = map.get(key);
+        touch(node);
+        return node.value;
+    }
+    
+    public void put(int key, int value) {
+        
+    }
+
+    private void touch(Node node) {
+        if(node == null) {
+            return;
+        }
+
+        final int prevFreq = node.freq;
+        final int freq = ++(node.freq);
+
+        List<Integer> list = this.listMap.get(prevFreq);
+        
+
+    }
+
+    private void printLine() {
+    	System.out.println("---------------------"); 
+	}
+
+    private void printString(String arg) {
+        System.out.println(arg); 
+    }    
+
+	private void printList(int[] list, int length) {
+		for(int i = 0; i < length; i ++ ){
+			System.out.println(list[i]);
+		}
+	}    
+
+	public static void main(String[] args) {
+
+
+	}
+
+}
