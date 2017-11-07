612d5ac37a7c16e51d7ff61ef4027284b08807d2
Zhen Gong <gongzhengz@gmail.com>
2017-11-06 23:04:34 -0800

    17, 42

---
+++ amazon/17_LetterCombinationsofPhoneNumber/Solution1.java
@@ -0,0 +1,184 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+// class TreeNode {
+//  int val;
+//  TreeNode left;
+//  TreeNode right;
+//  TreeNode(int x) { val = x; }
+// }
+// class ListNode {
+//     int val;
+//     ListNode next;
+//     ListNode(int x) {
+//         val = x;
+//         next = null;
+//     }
+// }
+// private TreeNode createTree() {
+//  //       5
+//  //    2      7
+//  //  1   3      10
+//  TreeNode node5 = new TreeNode(5);
+//  TreeNode node2 = new TreeNode(2);
+//  TreeNode node1 = new TreeNode(1);
+//  TreeNode node7 = new TreeNode(7);
+//  TreeNode node10 = new TreeNode(10);
+
+//  node5.left = node2;
+//  node5.right = node7;
+//  node2.left = node1;
+//  node2.right = node3;
+//  node5.right = node7;
+//  node7.right = node10; 
+//  return node5;
+// }
+// private void printList(List<String> list) {
+//  for(String s: list) {
+//      System.out.println(s);
+//  }
+// }
+// private void printQueue(Queue<String> list) {
+//  for(String s: list) {
+//      System.out.println(s);
+//  }
+// } 
+// private void enterKey() {
+//     System.out.println("Press \"ENTER\" to continue...");
+//     Scanner scanner = new Scanner(System.in);
+//     scanner.nextLine();      
+// }
+// private void printListOfList(List<List<Integer>> listOflist) {
+//  for(List<Integer> list: listOflist){
+//      for(int n : list) {
+//          System.out.print(n + ", ");
+//      }
+//      System.out.println();
+//  }
+// }
+// private void printMatrix(int[][] matrix) {
+//     for(int[] s: matrix) {
+//         for(int n: s) {
+//             System.out.print(n + ",");
+//         }
+//         System.out.println();            
+//     }
+// }  
+//  private void scannerIn() {
+//    Scanner scanner = new Scanner(System.in);
+//    scanner.nextLine();
+// }
+// private void printString(String arg) {
+//     System.out.println(arg); 
+// }
+// private void printLine() {
+//     System.out.println("---------------------"); 
+// }  
+// 
+// private void printMap(HashMap<TreeNode, Integer> map) {
+//     for(TreeNode node:map.keySet()) {
+//         Integer value = map.get(node);
+//         printString("key:" + node.val + ",value:" + value);
+//     }
+// }
+// private void printStringWithoutNewLine(String arg) {
+//     System.out.print(arg + ","); 
+// } 
+// private void printArray(int[] list) {
+//     for(int n: list) {
+//         System.out.println(n);
+//     }
+// } 
+// private void printStack(Stack<Integer> stack) {
+//     Interator<Integer> iter = stack.iterator();
+//     while(iter.hasNext()) {
+//         printString("" + iter.next());
+//     }
+// }
+// private void printTree(TreeNode root) {
+//     if(root == null) { return ; }
+//     Queue<TreeNode> queue = new LinkedList<TreeNode>();
+//     queue.offer(root);
+//     while(!queue.isEmpty()) {
+//         int size = queue.size();
+//         for(int i = 0; i < size; i++) {
+//             TreeNode node = queue.poll();                
+//             if(node.left != null) {
+//                 queue.offer(node.left);
+//             }
+//             if(node.right != null) {
+//                 queue.offer(node.right);
+//             }                
+//             printStringWithoutNewLine("" + node.val);
+//         }
+//         printLine();
+//     }
+// } 
+
+public class Solution1 {
+
+    public List<String> letterCombinations(String digits) {
+        if(digits.length() == 0) {
+            return new ArrayList<String>();
+        }
+        
+        List<String> res = new ArrayList<String>();
+        Map<Character, String> map = new HashMap<Character, String>();
+        map.put('2', "abc");
+        map.put('3', "def");
+        map.put('4', "ghi");
+        map.put('5', "jkl");
+        map.put('6', "mno");
+        map.put('7', "pqrs");
+        map.put('8', "tuv");
+        map.put('9', "wxyz");
+        
+        char[] cs = digits.toCharArray();
+        String sb = "";
+        dfs(res, cs, 0, sb, map);
+        return res;
+    }
+    
+    private void dfs(List<String> res, char[] cs, int pos, String sb, Map<Character, String> map) {    
+        if(sb.length() == cs.length){
+            printString("line147:" + sb);            
+            res.add(sb); 
+            return;
+        }
+        printString("line151:" + sb);
+        if(pos >= cs.length) {
+            return;
+        }        
+        
+        String str = map.get(cs[pos]);
+        for(int i = 0; i < str.toCharArray().length; i++) {
+            dfs(res, cs, pos + 1, sb + str.charAt(i), map);
+        }
+        
+    }
+
+    private int[][] direction = new int[][]{{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+
+    private void printLine() {
+    	System.out.println("---------------------"); 
+	}
+
+    private void printString(String arg) {
+        System.out.println(arg); 
+    }    
+
+	private void printList(int[] list, int length) {
+		for(int i = 0; i < length; i ++ ){
+			System.out.println(list[i]);
+		}
+	}    
+
+	public static void main(String[] args) {
+		Solution1 obj = new Solution1();
+        obj.letterCombinations("2");
+	}
+
+}
