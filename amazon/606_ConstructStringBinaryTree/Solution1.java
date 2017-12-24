e3ac996c8774ffb9981bb40a9062fe9eff2988c1
Zhen Gong <gongzhengz@gmail.com>
2017-12-24 11:09:14 -0800

    606

---
+++ amazon/606_ConstructStringBinaryTree/Solution1.java
@@ -0,0 +1,193 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+
+// private void printList(List<String> list) {
+//  for(String s: list) {
+//      System.out.println(s);
+//  }
+// }
+// private void printQueue(Queue<String> list) {
+//  for(String s: list) {
+//      System.out.println(s);
+//  }
+// } 
+// private void enterKey() {
+//     System.out.println("Press \"ENTER\" to continue...");
+//     Scanner scanner = new Scanner(System.in);
+//     scanner.nextLine();      
+// }
+// private void printListOfList(List<List<Integer>> listOflist) {
+//  for(List<Integer> list: listOflist){
+//      for(int n : list) {
+//          System.out.print(n + ", ");
+//      }
+//      System.out.println();
+//  }
+// }
+// private void printMatrix(int[][] matrix) {
+//     for(int[] s: matrix) {
+//         for(int n: s) {
+//             System.out.print(n + ",");
+//         }
+//         System.out.println();            
+//     }
+// }  
+//  private void scannerIn() {
+//    Scanner scanner = new Scanner(System.in);
+//    scanner.nextLine();
+// }
+// private void printString(String arg) {
+//     System.out.println(arg); 
+// }
+// private void printLine() {
+//     System.out.println("---------------------"); 
+// }  
+// 
+// private void printMap(HashMap<TreeNode, Integer> map) {
+//     for(TreeNode node:map.keySet()) {
+//         Integer value = map.get(node);
+//         printString("key:" + node.val + ",value:" + value);
+//     }
+// }
+// private void printStringWithoutNewLine(String arg) {
+//     System.out.print(arg + ","); 
+// } 
+// private void printArray(int[] list) {
+//     printStringWithoutNewLine("[");
+//     for(int n: list) {
+//         printStringWithoutNewLine(n + ", ");
+//     }
+//     printStringWithoutNewLine("]\n");
+// } 
+// private void printArray(int[] list) {
+//     printStringWithoutNewLine("[");
+//     int i = 0;
+//     for(int n : list) {
+//         printStringWithoutNewLine("[" + i + "]" + n + ", ");
+//         i++;
+//     }
+//     printStringWithoutNewLine("]\n");
+// }  
+// private void printArray(int[] list, int s, int e) {
+//     printStringWithoutNewLine("[");
+//     for(int i = s; i <= e; i++) {
+//         printStringWithoutNewLine("[" + i + "]" + list[i] + ", ");
+//     }
+//     printStringWithoutNewLine("]\n");
+// } 
+// private void printStringWithoutNewLine(String arg) {
+//     System.out.print(arg); 
+// }  
+// private void printStringWithoutNewLine(String arg) {
+//     System.out.print(arg); 
+// } 
+// private void printStack(Stack<Integer> stack) {
+//     Interator<Integer> iter = stack.iterator();
+//     while(iter.hasNext()) {
+//         printString("" + iter.next());
+//     }
+// }
+// private void printTree(TreeNode root) {
+//     if(root == null) { return ; }
+//     Queue<TreeNode> queue = new LinkedList<TreeNode>();
+//     queue.offer(root);
+//     while(!queue.isEmpty()) {
+//         int size = queue.size();
+//         for(int i = 0; i < size; i++) {
+//             TreeNode node = queue.poll();                
+//             if(node.left != null) {
+//                 queue.offer(node.left);
+//             }
+//             if(node.right != null) {
+//                 queue.offer(node.right);
+//             }                
+//             printStringWithoutNewLine("" + node.val);
+//         }
+//         printLine();
+//     }
+// } 
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class Solution1 {
+
+    public String tree2str(TreeNode t) {
+        StringBuilder sb = new StringBuilder();
+        dfs(t, sb);
+        return sb.toString();
+    }
+    
+    private String dfs(TreeNode t, StringBuilder sb) {
+        if(t == null) {
+            return "";
+        }
+        sb.append("" + t.val);
+        if(t.left != null) {
+            sb.append("(");
+            sb.append("" + dfs(t.left, new StringBuilder()));
+            sb.append(")");
+        } 
+        if(t.right != null) {
+            sb.append("(");
+            sb.append("" + dfs(t.right, new StringBuilder()));
+            sb.append(")");            
+        }
+        return sb.toString();
+    }	
+
+    private TreeNode createTree() {
+        //       5
+        //    2      7
+        //  1   3      10
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node4 = new TreeNode(4);
+
+        node1.left = node2;
+        node1.right = node3;
+        node2.right = node4;        
+        return node1;
+    }    
+
+    private int[][] direction = new int[][]{{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+
+    private void printLine() {
+    	System.out.println("---------------------"); 
+	}
+
+    private void printString(String arg) {
+        System.out.println(arg); 
+    }    
+
+	private void printList(int[] list, int length) {
+		for(int i = 0; i < length; i ++ ){
+			System.out.println(list[i]);
+		}
+	}    
+
+	public static void main(String[] args) {
+		Solution1 obj = new Solution1();
+		TreeNode root = obj.createTree();
+		System.out.println(obj.tree2str(root));
+	}
+
+}
