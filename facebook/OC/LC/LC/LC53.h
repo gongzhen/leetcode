9fdcc57b27526ddc6b04ef660d3c79a3463b13f3
ULS <gongzhengz@gmail.com>
2018-02-23 21:45:02 -0800

    LC53

---
+++ facebook/OC/LC/LC/LC53.h
@@ -0,0 +1,13 @@
+//
+//  LC53.h
+//  LC
+//
+//  Created by ULS on 2/23/18.
+//  Copyright © 2018 ULS. All rights reserved.
+//
+
+#import "LC.h"
+
+@interface LC53 : LC
+
+@end
---
+++ facebook/OC/LC/LC/LC53.m
@@ -0,0 +1,13 @@
+//
+//  LC53.m
+//  LC
+//
+//  Created by ULS on 2/23/18.
+//  Copyright © 2018 ULS. All rights reserved.
+//
+
+#import "LC53.h"
+
+@implementation LC53
+
+@end
