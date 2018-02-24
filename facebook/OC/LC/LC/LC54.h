867ff2baf9abb9eb92c298c2ce7f49e4425e2982
ULS <gongzhengz@gmail.com>
2018-02-23 17:05:09 -0800

    LC54

---
+++ facebook/OC/LC/LC/LC54.h
@@ -0,0 +1,13 @@
+//
+//  LC54.h
+//  LC
+//
+//  Created by ULS on 2/23/18.
+//  Copyright © 2018 ULS. All rights reserved.
+//
+
+#import "LC.h"
+
+@interface LC54 : LC
+
+@end
---
+++ facebook/OC/LC/LC/LC54.m
@@ -0,0 +1,13 @@
+//
+//  LC54.m
+//  LC
+//
+//  Created by ULS on 2/23/18.
+//  Copyright © 2018 ULS. All rights reserved.
+//
+
+#import "LC54.h"
+
+@implementation LC54
+
+@end
