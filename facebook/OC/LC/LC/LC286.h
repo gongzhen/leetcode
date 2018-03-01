1c5f828a7e3fee28774fec01c2380fee550a45f2
gongzhen <gongzhengz@gmail.com>
2018-02-28 18:59:08 -0800

    LC286

---
+++ facebook/OC/LC/LC/LC286.h
@@ -0,0 +1,13 @@
+//
+//  LC286.h
+//  LC
+//
+//  Created by Admin  on 2/28/18.
+//  Copyright © 2018 ULS. All rights reserved.
+//
+
+#import "LC.h"
+
+@interface LC286 : LC
+
+@end
---
+++ facebook/OC/LC/LC/LC286.m
@@ -0,0 +1,13 @@
+//
+//  LC286.m
+//  LC
+//
+//  Created by Admin  on 2/28/18.
+//  Copyright © 2018 ULS. All rights reserved.
+//
+
+#import "LC286.h"
+
+@implementation LC286
+
+@end
