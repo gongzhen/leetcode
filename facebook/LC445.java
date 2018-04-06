c1ee65769845ae7ed15779087f4e98eb2466abf6
gongzhen <gongzhengz@gmail.com>
2018-04-05 17:05:37 -0700

    LC445

---
+++ facebook/LC445.java
@@ -0,0 +1,242 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+// class TreeNode {
+//  int val;
+//  TreeNode left;
+//  TreeNode right;
+//  TreeNode(int x) { val = x; }
+// }
+
+// class Interval {
+//     int start;
+//     int end;
+//     Interval() { start = 0; end = 0; }
+//     Interval(int s, int e) { start = s; end = e; }
+// }
+// private TreeNode createTree() {
+//     //       5
+//     //    2      7
+//     //  1   3      10
+//     TreeNode node5 = new TreeNode(5);
+//     TreeNode node2 = new TreeNode(2);
+//     TreeNode node1 = new TreeNode(1);
+//     TreeNode node7 = new TreeNode(7);
+//     TreeNode node3 = new TreeNode(3);
+//     TreeNode node10 = new TreeNode(10);
+//     node5.left = node2;
+//     node5.right = node7;
+//     node2.left = node1;
+//     node2.right = node3;
+//     node5.right = node7;
+//     node7.right = node10; 
+//     return node5;
+// }  
+// private void printList(List<String> list) {
+//  for(String s: list) {
+//      System.out.println(s);
+//  }
+// }
+// private void printListMap(Map<Integer, List<Integer>> map) {
+//     for(Integer key:map.keySet()) {
+//         List<Integer> list = map.get(key);
+//         printStringWithoutNewLine("key:" + key + "[");   
+//         for(int n : list) {
+//             printStringWithoutNewLine(n + ", ");    
+//         }
+//         printString("]");               
+//     }
+// }
+// private void printQueue(Queue<String> list) {
+//  for(String s: list) {
+//      System.out.println(s);
+//  }
+// } 
+// private void enterKey() {
+//     System.out.println("Press \"ENTER\" to continue...");
+//     Scanner scanner = new Scanner(System.in);
+//     scanner.nextLine();      
+// }
+// private void printListOfList(List<List<Integer>> listOflist) {
+//  for(List<Integer> list: listOflist){
+//      for(int n : list) {
+//          System.out.print(n + ", ");
+//      }
+//      System.out.println();
+//  }
+// }
+// private void printMatrix(int[][] matrix) {
+//     for(int[] s: matrix) {
+//         for(int n: s) {
+//             System.out.print(n + ",");
+//         }
+//         System.out.println();            
+//     }
+// }  
+//  private void scannerIn() {
+//    Scanner scanner = new Scanner(System.in);
+//    scanner.nextLine();
+// }
+// private void printString(String arg) {
+//     System.out.println(arg); 
+// }
+// private void printLine() {
+//     System.out.println("---------------------"); 
+// }  
+// 
+// private void printMap(HashMap<TreeNode, Integer> map) {
+//     for(TreeNode node:map.keySet()) {
+//         Integer value = map.get(node);
+//         printString("key:" + node.val + ",value:" + value);
+//     }
+// }
+// private void printArray(int[] list) {
+//     printStringWithoutNewLine("[");
+//     for(int n: list) {
+//         printStringWithoutNewLine(n + ", ");
+//     }
+//     printStringWithoutNewLine("]\n");
+// } 
+// private void printArray(int[] list) {
+//     printStringWithoutNewLine("[");
+//     int i = 0;
+//     for(int n : list) {
+//         printStringWithoutNewLine("[" + i + "]" + n + ", ");
+//         i++;
+//     }
+//     printStringWithoutNewLine("]\n");
+// }  
+// private void printArray(int[] list, int s, int e) {
+//     printStringWithoutNewLine("[");
+//     for(int i = s; i <= e; i++) {
+//         printStringWithoutNewLine("[" + i + "]" + list[i] + ", ");
+//     }
+//     printStringWithoutNewLine("]\n");
+// } 
+// private void printStringWithoutNewLine(String arg) {
+//     System.out.print(arg); 
+// }  
+// private void printStringWithoutNewLine(String arg) {
+//     System.out.print(arg); 
+// } 
+// private void printStack(Stack<Integer> stack) {
+//     Interator<Integer> iter = stack.iterator();
+//     while(iter.hasNext()) {
+//         printString("" + iter.next());
+//     }
+// }
+// private void printTree(TreeNode root) {
+//     if(root == null) { return ; }
+//     Queue<TreeNode> queue = new LinkedList<TreeNode>();
+//     queue.offer(root);
+//     while(!queue.isEmpty()) {
+//         int size = queue.size();
+//         for(int i = 0; i < size; i++) {
+//             TreeNode node = queue.poll();                
+//             if(node.left != null) {
+//                 queue.offer(node.left);
+//             }
+//             if(node.right != null) {
+//                 queue.offer(node.right);
+//             }                
+//             printStringWithoutNewLine("" + node.val);
+//         }
+//         printLine();
+//     }
+// } 
+
+class ListNode {
+    int val;
+        ListNode next;
+        ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class LC25 {
+
+    public ListNode createList() {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        // ListNode node3 = new ListNode(3);
+        // ListNode node4 = new ListNode(4);
+        // ListNode node5 = new ListNode(5);
+        // ListNode node6 = new ListNode(6);
+        // ListNode node7 = new ListNode(7);
+        node1.next = node2;
+        // node2.next = node3;
+        // node3.next = node4;
+        // node4.next = node5;
+        // node5.next = node6;
+        // node6.next = node7;
+        return node1;
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        Stack<ListNode> s1 = new Stack<ListNode>();
+        Stack<ListNode> s2 = new Stack<ListNode>();
+        while(l1 != null) {
+            s1.push(l1);
+            l1 = l1.next;
+        }
+        while(l2 != null) {
+            s2.push(l2);
+            l2 = l2.next;
+        }        
+        
+        ListNode head = new ListNode(0);
+        ListNode prev = null;
+        int carry = 0;
+        int sum = 0;
+        while(!s1.isEmpty() || !s2.isEmpty()) {
+            int n1 = s1.isEmpty() == true? 0 : s1.pop().val;
+            int n2 = s2.isEmpty() == true? 0 : s2.pop().val;
+            sum = (n1 + n2 + carry) % 10;
+            carry = (n1 + n2 + carry) / 10;
+            ListNode node = new ListNode(sum);
+            head = node;
+            head.next = prev;
+            prev = node;
+        }
+        return head;            
+    }
+
+    private int[][] direction = new int[][]{{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+
+    private void printLine() {
+    	System.out.println("---------------------"); 
+	}
+
+    private void printString(String arg) {
+        System.out.println(arg); 
+    }    
+
+	private void printList(ListNode node) {
+        ListNode ptr = node;
+        printStringWithoutNewLine("[ ");
+        while(ptr != null) {
+            printStringWithoutNewLine(ptr.val + ", ");
+            ptr = ptr.next;
+        }
+        printStringWithoutNewLine("]\n");
+	}    
+
+    private void printStringWithoutNewLine(String arg) {
+        System.out.print(arg + ","); 
+    } 
+
+
+	public static void main(String[] args) {
+		LC25 obj = new LC25();
+        ListNode root = obj.createList();
+        // obj.reverseKGroup(root, 2);
+        // obj.reverseKGroup(root, 3);
+        // obj.reverseKGroup(root, 8);
+        // obj.addTwoNumbers(root, 1);
+	}
+
+}
