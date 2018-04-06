bb5c11c83a95ada1a4440446609dd967234141a0
gongzhen <gongzhengz@gmail.com>
2018-04-05 17:54:28 -0700

    LC53

--- facebook/OC/LC/LC/LC53.m
+++ facebook/OC/LC/LC/LC53.m
@@ -18,6 +18,14 @@
 
 @implementation LC53
 
+/**
+ 53. Maximum Subarray
+ Find the contiguous subarray within an array (containing at least one number) which has the largest
+ sum.
+ For example, given the array [-2,1,-3,4,-1,2,1,-5,4],
+ the contiguous subarray [4,-1,2,1] has the largest sum = 6.
+ 
+ */
 - (int)maxSubArray2:(int *)nums size:(int)size {
     if(size == 0) {
         return 0;
