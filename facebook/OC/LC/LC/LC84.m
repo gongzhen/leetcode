1a08d16a2b5c3cfd56614708735d9e989b402feb
ULS <gongzhengz@gmail.com>
2018-04-22 13:51:47 -0700

    LC84

--- facebook/OC/LC/LC/LC84.m
+++ facebook/OC/LC/LC/LC84.m
@@ -7,8 +7,38 @@
 //
 
 #import "LC84.h"
+#import "NSMutableArray+Stack.h"
 
 @implementation LC84
+
+/// https://leetcode.com/problems/largest-rectangle-in-histogram/discuss/28900/O(n)-stack-based-JAVA-solution
+- (int)largestRectangleAreaStack:(int* )heights size:(int)size {
+    if(heights == nil || size == 0) {
+        return 0;
+    }
+    int max = 0;
+    NSMutableArray *stack = [NSMutableArray array];
+    for(int i = 0; i <= size;) {
+        int height = (i == size) ? 0 : heights[i];
+        /// stack push index when the hight is ascending.
+        if(stack.count == 0 || height >= heights[[[stack peek] intValue]]) {
+            [stack push:@(i)];
+            i++;
+        } else {
+            /// height is descending.
+            int index = [[stack pop] intValue];
+            int leftHeight = heights[index];
+            /// Comment line 31 to 34 and it still passes the leetcode.
+            while( stack.count != 0 && leftHeight == heights[[[stack peek] intValue]]) {
+                [stack pop];
+            }
+            
+            int leftMost = stack.count == 0 ? 0 : [[stack peek] intValue] + 1;
+            max = MAX(max, leftHeight * (i - leftMost));
+        }
+    }
+    return max;
+}
 
 - (int)largestRectangleArea:(int* )heights size:(int)size {
     if(heights == nil || size == 0) {
@@ -31,7 +61,7 @@
 - (void)test {
     int num1[6] = {2,1,5,6,2,3};
     int *ptr = num1;
-    int res = [self largestRectangleArea:ptr size:6];
+    int res = [self largestRectangleAreaStack:ptr size:6];
     DLog(@"res:%d", res);
 }
 @end
