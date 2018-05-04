15a9c4c09b0b1c72870084aeeb7d4e5c8dcf8ceb
Zhen Gong <gongzhengz@gmail.com>
2018-05-03 17:55:32 -0700

    LC494

---
+++ LCIntelliJ/src/LC494.java
@@ -0,0 +1,34 @@
+public class LC494 {
+
+    public int findTargetSumWays(int[] nums, int s) {
+
+        int sum = 0;
+        for(int n : nums) {
+            sum += n;
+        }
+
+        if(s > sum || s < -sum) {
+            return 0;
+        }
+
+        int[][] dp = new int[nums.length + 1][2 * sum + 1];
+        dp[0][0 + sum] = 1;
+        for(int i = 1; i < nums.length + 1; i++) {
+            for(int j = 1; j < 2 * sum + 1; j++) {
+                if(j + nums[i - 1] < 2 * sum + 1) {
+                    dp[i][j] += dp[i - 1][j + nums[i - 1]];
+                }
+                if(j - nums[i - 1] >= 0) {
+                    dp[i][j] += dp[i - 1][j - nums[i - 1]];
+                }
+            }
+        }
+        return dp[nums.length][sum + s];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 1, 1, 1, 1};
+        LC494 obj = new LC494();
+        obj.findTargetSumWays(nums, 3);
+    }
+}
